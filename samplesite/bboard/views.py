0f4f7a7b89091aa700b7dbd66f1e00b372a881db
saharovartur <saharovartur87@gmail.com>
2024-03-01 23:15:21 +0400

    1. Задал именованные параметры атрибутов-полей в бд в models. 2. создал класс Meta и задал настройки самой модели. 3. Зарегистрировал модели для отображения в админки в модуле admin. 4. Изменил метод в функции index в views.py, так как за сортировку теперь отвечает класс Meta

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Bb
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all()
     return render(request, 'bboard/index.html', {'bbs': bbs})
 
 

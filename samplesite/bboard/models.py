49af6ddeeca4894b8ae9b3a538d6ff3be9cf63bd
saharovartur <saharovartur87@gmail.com>
2024-03-02 17:55:31 +0400

    Создал модель Rubric - с одним полем Рубрика, настроил связь "один ко многим" с моделью Bb. Это позволить держать объявления в своих категориях.

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
+
 class Bb(models.Model):
+    rubric = models.ForeignKey(Rubric, null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(verbose_name='Описание')
     price = models.FloatField(verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+
 
     class Meta:
         verbose_name_plural = 'Объявления'

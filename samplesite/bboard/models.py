63a2572a371f6970a35f1dd24a707e6e4418c90c
saharovartur <saharovartur87@gmail.com>
2024-03-02 19:44:58 +0400

    Записал в класс Rubric магический метод str для возврат объекта в виде строки для корректного отображения названия категорий в админке.

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -3,14 +3,17 @@ from django.db import models
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
+
 
     class Meta:
-        verbose_name_plural = 'Рубрики'
-        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Категории'
+        verbose_name = 'Категория'
         ordering = ['name']
 
 class Bb(models.Model):
-    rubric = models.ForeignKey(Rubric, null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+    rubric = models.ForeignKey(Rubric, null=True, on_delete=models.PROTECT, verbose_name='Категория')
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(verbose_name='Описание')
     price = models.FloatField(verbose_name='Цена')

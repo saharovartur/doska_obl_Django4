1f5bc8ea41ed989c5a8acf0bbe9886d4e6dfc69b
saharovartur <saharovartur87@gmail.com>
2024-03-03 18:11:19 +0400

    добавицл функцию представление для обработки и вывода информации из модели Rubric, настроил фильтрацию в переменных и связь с HTMl-страницей. Изменил функцию index - добавил туда переменную rubrics для отображения на главной странице рубрик, изменил переменную context.

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,10 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Bb
+from .models import Bb, Rubric
 
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+def rubric_bbs(request, rubric_id):
+    bbs = Bb.objects.filter(rubric_id=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(id=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+               'current_rubric': current_rubric}
+    return render(request, 'bboard/rubric_bbs.html', context)
+
+
+
+
 
 
 

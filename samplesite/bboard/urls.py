676e96bdf0e695c2ac2253da6b0cfd45c3c8b429
saharovartur <saharovartur87@gmail.com>
2024-03-03 18:49:06 +0400

    добавил имена маршрутов - создал именованные маршруты

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import index, rubric_bbs
 
 urlpatterns = [
-    path('', index),
-    path('<int:rubric_id>/', rubric_bbs),
+    path('', index, name='index'),
+    path('<int:rubric_id>/', rubric_bbs, name='rubric_bbs'),
 
 
 ]

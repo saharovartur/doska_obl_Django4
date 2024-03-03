50af124bfec534ab7617d7e6de99a0a7fa3915c8
saharovartur <saharovartur87@gmail.com>
2024-03-03 21:24:44 +0400

    Создал файл. Создал форму BbForm для связи с моделью Bb служащую для создания новых объявлений на стороне клиента.

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -2,6 +2,9 @@ from django.forms import ModelForm
 from .models import Bb
 
 class BbForm(ModelForm):
+    """Форма  BbForm для связи с моделью Bb.
+    Служит для внесения новых объявлений на стороне клиента.
+    """
     class Meta:
         model = Bb
         fields = ('title', 'content', 'price', 'rubric')

1283e0756b3ae8bd54cada5bf3cf61702ec536b5
saharovartur <saharovartur87@gmail.com>
2024-03-03 18:08:17 +0400

    добавил новый URL-адрес для связи с представлением rubric_bbs

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, rubric_bbs
 
 urlpatterns = [
     path('', index),
+    path('<int:rubric_id>/', rubric_bbs),
 
 
 ]

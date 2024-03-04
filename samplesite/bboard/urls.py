6ac6a2b521ddb466c8563a3bb1997611fd055e2e
saharovartur <saharovartur87@gmail.com>
2024-03-04 22:28:31 +0400

    Определил адрес URL add для обработки запроса страницы с формой- добавить новое объявление

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import index, rubric_bbs
+from .views import index, rubric_bbs, BbCreateView
 
 urlpatterns = [
     path('', index, name='index'),
     path('<int:rubric_id>/', rubric_bbs, name='rubric_bbs'),
-
-
+    path('add/', BbCreateView.as_view(), name='add'),
 ]

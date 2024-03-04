58e4b23092a7135652611b0e9b29b30a628af5a6
saharovartur <saharovartur87@gmail.com>
2024-03-04 19:19:08 +0400

    добавил комментарии к моделям

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 
+
 class Rubric(models.Model):
+    """Модель 'Рубрики' хранит один атрибут с полем названия рубрики"""
     name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
 
     def __str__(self):
         return self.name
 
-
     class Meta:
+        """Настроечный класс для админки."""
         verbose_name_plural = 'Категории'
         verbose_name = 'Категория'
         ordering = ['name']
 
 class Bb(models.Model):
+    """Модель 'Объявления'  хранит атрибуты с столбцами таблицы"""
     rubric = models.ForeignKey(Rubric, null=True, on_delete=models.PROTECT, verbose_name='Категория')
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(verbose_name='Описание')
